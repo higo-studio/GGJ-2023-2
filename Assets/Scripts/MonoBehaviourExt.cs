9ea729e8904b606e5abe68428f308e8d980e899c
mooooooi <emwings@outlook.com>
2023-02-04 14:14:51 +0800

    feat: 基础PlayerInput

---
+++ Assets/Scripts/MonoBehaviourExt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class MonoBehaviourExt
+{
+    public static void StartCoroutineRef(this MonoBehaviour mono, ref Coroutine coroutine, IEnumerator enumerator)
+    {
+        if (coroutine != null)
+        {
+            mono.StopCoroutine(coroutine);
+        }
+        coroutine = mono.StartCoroutine(enumerator);
+    }
+}
