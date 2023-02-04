9f4cb327ec54c69e72b2beee8876f0109623cc40
mooooooi <emwings@outlook.com>
2023-02-04 19:42:05 +0800

    feat: 增加未找到的配置提示

--- Assets/Scripts/PlatformIcon.cs
+++ Assets/Scripts/PlatformIcon.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,6 +22,9 @@ public class PlatformIcon : MonoBehaviour
     }
     [SerializeField]
     private eIconType m_iconType;
+
+    private eIconType m_cacheIconType;
+    private string m_cacheDeviceName;
     void Awake()
     {
         m_image = GetComponent<Image>();
@@ -38,6 +41,10 @@ public class PlatformIcon : MonoBehaviour
     public void Refresh()
     {
         if (m_image == null) return;
+        if (m_cacheDeviceName == PlayerInput.s_LastDeviceDisplayName && m_cacheIconType == m_cacheIconType) return;
+
+        m_cacheDeviceName = PlayerInput.s_LastDeviceDisplayName;
+        m_cacheIconType = m_cacheIconType;
         Sprite sprite = null;
         if (iconCollections.Map.TryGetValue(m_iconType, out var mapper))
         {
@@ -59,6 +66,7 @@ public class PlatformIcon : MonoBehaviour
                         sprite = mapper.Switch;
                         break;
                     default:
+                        Debug.Log($"未找到适配的配置: {deviceName}");
                         sprite = mapper.KeyBoardWASD;
                         break;
                 }
