e8970b0953da9475ba15b90705462b02d578da21
paoMian <55372434+panda8246@users.noreply.github.com>
2023-02-05 16:02:05 +0800

    字体

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Knight : MonoBehaviour
+{
+    private Animator animator;
+    
+
+}
